51900696503c9dfcc49476fa161b2d764d22cb71
= <geraldomartins10@gmail.com>
2021-09-15 00:28:09 -0300

    Criação do arquivo de teste de DoubleArray

---
+++ src/main/kotlin/com/juniormoura/collectios/TesteDoubleArray.kt
@@ -0,0 +1,27 @@
+package com.juniormoura.collectios
+
+fun main() {
+    val salarios = DoubleArray(3)
+    salarios[0] = 1000.0
+    salarios[1] = 3000.0
+    salarios[2] = 500.0
+
+    salarios.forEach { println(it) }
+
+    println("-------------------------")
+
+   salarios.forEachIndexed { index, salario ->
+       salarios[index] = salario * 1.1
+   }
+    salarios.forEach { println(it) }
+
+    println("-------------------------")
+
+    val salarios2 = doubleArrayOf(1500.0, 2700.0, 560.0)
+    salarios2.sort()
+    salarios2.forEach { println(it)}
+
+
+}
+
+
