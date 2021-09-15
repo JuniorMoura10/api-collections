b653dedf1c2385af2a9fadf1a422bc7125f462e4
= <geraldomartins10@gmail.com>
2021-09-14 23:56:35 -0300

    Criação do arquivo de teste de ArrayString

---
+++ src/main/kotlin/com/juniormoura/collectios/TesteArrayString.kt
@@ -0,0 +1,23 @@
+package com.juniormoura.collectios
+
+fun main() {
+    val nomes = Array(3){""}
+    nomes[0] = "Maria"
+    nomes[1] = "Zaza"
+    nomes[2] = "Pedro"
+
+    for(nome in nomes){
+        println(nome)
+    }
+
+    println("---------------------")
+
+    nomes.sort()
+    nomes.forEach { println(it) }
+
+    println("---------------------")
+
+    val nomes2 = arrayOf("Maria", "Zaza", "Pedro")
+    nomes2.sort()
+    nomes2.forEach { println(it) }
+}
