3268a367052f7605198798fc6b7392c670155430
= <geraldomartins10@gmail.com>
2021-09-16 23:22:22 -0300

    mais teste de operações com array (count, find e any)

--- src/main/kotlin/com/juniormoura/collectios/TesteOperacoes.kt
+++ src/main/kotlin/com/juniormoura/collectios/TesteOperacoes.kt
@@ -13,8 +13,18 @@ fun main() {
     println("Menor salario: ${salarios.minOrNull()}")
     println("Média salario: ${salarios.average()}")
 
-    val salariosMaiorQue2500 = salarios.filter { it > 2500.0}
     println("-------------------------")
-
+    val salariosMaiorQue2500 = salarios.filter {it > 2500.0}
     salariosMaiorQue2500.forEach { println(it) }
+
+    println("-------------------------")
+    println(salarios.count { it in 2000.0..5000.0 })
+
+    println("-------------------------")
+    println(salarios.find { it == 2000.0 })
+    println(salarios.find { it == 3000.0 })
+
+    println("-------------------------")
+    println(salarios.any { it == 2000.0 })
+    println(salarios.any { it == 3000.0 })
 }
